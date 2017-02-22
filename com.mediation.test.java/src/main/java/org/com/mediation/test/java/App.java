da5339e0fdc00286c0122677d4ed9330b22ae6f8
manoj <manojg@wwso2.com>
2017-02-22 18:04:03 +0530

    test release

---
+++ com.mediation.test.java/src/main/java/org/com/mediation/test/java/App.java
@@ -0,0 +1,13 @@
+package org.com.mediation.test.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
